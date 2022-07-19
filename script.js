fdcfffeef23e77b441fd8a0fa199cdfffc30daca
Rahul Tyagi <rahultyagi1307@gmail.com>
2022-07-19 15:20:05 +0530

    commit change

--- script.js
+++ script.js
@@ -31,3 +31,23 @@ function calculate(){
     document.getElementById("result").value = answer;
     flag = 1;
 }
+document.addEventListener('keydown', (event) => {
+    if (event.key == "0") {document.getElementById("result").value += "0";}
+    else if (event.key == "1") {document.getElementById("result").value += "1";}
+    else if (event.key == "2") {document.getElementById("result").value += "2";}
+    else if (event.key == "3") {document.getElementById("result").value += "3";}
+    else if (event.key == "4") {document.getElementById("result").value += "4";}
+    else if (event.key == "5") {document.getElementById("result").value += "5";}
+    else if (event.key == "6") {document.getElementById("result").value += "6";}
+    else if(event.key == "7") {document.getElementById("result").value += "7";}
+    else if(event.key == "8") {document.getElementById("result").value += "8";}
+    else if(event.key == "9") {document.getElementById("result").value += "9";}
+    else if(event.key == "+") {document.getElementById("result").value += "+";}
+    else if(event.key == "-") {document.getElementById("result").value += "-";}
+    else if(event.key == "*") {document.getElementById("result").value += "*";}
+    else if(event.key == "/") {document.getElementById("result").value += "/";}
+    else if(event.key == ".") {document.getElementById("result").value += ".";}
+    else if(event.key == "=") {calculate();}
+    else if(event.key == "Enter") {calculate();}
+    else if(event.key == "Backspace") {clearElement();}
+});
